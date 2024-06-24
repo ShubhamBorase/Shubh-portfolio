e0738793a9bb7c521de4266a7b4cdb17fe9779be
ShubhamPB <shubhamborase467@gmail.com>
2024-06-24 08:31:26 +0530

    Commiting first port

---
+++ Portfolio Website/src/script.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text-animation",{
+    strings : ["Back-end Developer", "Engineer", "Computer Technocian", "Student"],
+    typedSpeed : 150,
+    backSpeed : 150,
+    looped : true
+}
+);
